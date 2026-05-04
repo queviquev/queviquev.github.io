ece0c4a496d39c38112404f1cfb52e378bea7f1b
queviquev <larryk.resendiz@gmail.com>
2026-05-04 18:01:18 -0500

    css fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,28 @@ import About from "./components/About";
 import './components/styles/about.css';
 import Portfolio from "./components/Portfolio";
 import './components/styles/portfolio.css';
-// import Contact from "./components/Contact";
 import './components/styles/contact.css';
 import Footer from "./components/Footer";
 import './components/styles/footer.css';
 import Sidebar from "./components/Sidebar";
 import './components/styles/sidebar.css';
 import './components/LoadingScreen';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './components/styles/styles.css'
 import LoadingScreen from "./components/LoadingScreen";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Sidebar/>
-      <LoadingScreen />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>      
+      
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Sidebar/>
+        <LoadingScreen />      
         <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Intro/>} />
+          <Route path="/about" element={<About/>} />
           <Route path="/portfolio" element={<Portfolio/>} />
-          {/* <Route path="/contact" element={<Contact/>} /> */}
         </Routes>
         <Footer/>
       </BrowserRouter>
